cfea77a4c56db828dca4b6619c34c1f5621be414
dcidcoronado <daniel.cid.coronado@gmail.com>
2021-06-28 23:14:09 -0400

    entrega

--- leagues/views.py
+++ leagues/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
+from django.db.models import Count
 
 from . import team_maker
 
@@ -42,46 +43,45 @@ def index(request):
 	# 1. todos los equipos en la Atlantic Women's Soccer Federeation
 	awsf_teams = Team.objects.filter(league__name="Atlantic Womens' Soccer Federation")
 	# 2. todos los jugadores (actuales) en los Raleigh Penguins
-	rp_players = Player.objects.filter(curr_team__team_name="Penguins")
+	rp_players = Player.objects.filter(curr_team__team_name="Penguins", curr_team__location="Raleigh")
 	# 3. todos los jugadores (actuales) en la International Collegiate Football Association
 	icfa_players = Player.objects.filter(curr_team__league__name="International Collegiate Football Association")
 	# 4. todos los jugadores (actuales) en la Conferencia Americana de Fútbol Amateur con el apellido "Wilson"
-	acaf_players = Player.objects.filter(curr_team__league__name="American Conference of Amateur Football")
-	wilson_acaf = acaf_players.filter(last_name="Wilson")
+	wilson_acaf = Player.objects.filter(curr_team__league__name="American Conference of Amateur Football", last_name="Wilson")
 	# 5. todos los jugadores de fútbol
 	football_players = Player.objects.filter(curr_team__league__sport="Football")
 	# 6. todos los equipos con un jugador (actual) llamado "Sophia"
 	sophia_teams = Team.objects.filter(curr_players__first_name="Sophia")
 	# 7. todas las ligas con un jugador (actual) llamado "Sophia"
 	sophia_leagues = League.objects.filter(teams__curr_players__first_name="Sophia")
-	# 8. todos con el apellido "Flores" que NO (actualmente) juegan para los DC Nuggets
-	dcn_players = Player.objects.filter(curr_team__team_name="Nuggets")
-	noflores_dcn = dcn_players.exclude(last_name="Flores")
-	# 9. todos los equipos, pasados y presentes, con los que Samuel Evans ha jugado
-	e_teams = Team.objects.filter(all_players__last_name="Evans")
-	ce_teams = e_teams.filter(all_players__first_name="Charlotte")
+	# 8. todos los jugadores con el apellido "Flores" que NO (actualmente) juegan para los DC Nuggets
+	flores_nodcn = Player.objects.filter(last_name="Flores").exclude(curr_team__team_name="Nuggets")
+	# 9. todos los equipos, pasados y presentes, con los que Charlotte Evans ha jugado
+	ce_teams = Team.objects.filter(all_players__last_name="Evans", all_players__first_name="Charlotte")
 	# 10. todos los jugadores, pasados y presentes, de Calgary Generals
-	cg_players = Player.objects.filter(all_teams__team_name="Generals")
+	cg_players = Player.objects.filter(all_teams__team_name="Generals", all_teams__location="Calgary")
 	# 11. todos los jugadores que anteriormente estaban (pero que no lo están) con los Wisconsin White Sox
-	wws_players = Player.objects.filter(all_teams__team_name="White Sox")
-	old_wws_players = wws_players.exclude(curr_team__team_name="White Sox")
+	old_wws_players = Player.objects.filter(all_teams__team_name="White Sox").exclude(curr_team__team_name="White Sox")
 	# 12. cada equipo para el que Christian Roberts jugó antes de unirse a los Chicago Cubs
-	r_teams = Team.objects.filter(all_players__last_name="Roberts")
-	cr_teams = r_teams.filter(all_players__first_name="Christian")
-	cr_oldteams = cr_teams.exclude(team_name="Cubs")
+	cr_oldteams = Team.objects.filter(all_players__last_name="Roberts", all_players__first_name="Christian").exclude(team_name="Cubs", location="Chicago")
 	# 13. todos llamados "Joshua" que alguna vez han jugado en la Pacific Basketball Athletics Conference
-	pbac_players = Player.objects.filter(all_teams__league__name="Pacific Basketball Athletics Conference")
-	pbac_joshua_players = pbac_players.filter(first_name="Joshua")
-
-
+	pbac_joshua_players = Player.objects.filter(all_teams__league__name="Pacific Basketball Athletics Conference", first_name="Joshua")
+	# 14. todos los equipos que han tenido 12 o más jugadores, pasados y presentes
+	twelveplus_teams = Team.objects.annotate(Count('all_players'))
+	# 15. todos los jugadores y el número de equipos para los que jugó, ordenados por la cantidad de equipos para los que han jugado
+	players_teams = Player.objects.annotate(num_teams=Count('all_teams')).order_by('-num_teams')
+	
+	
 	context = {
 		#SPORTS ORM II
+		'players_teams': players_teams,
+		'twelveplus_teams': twelveplus_teams,
 		'pbac_joshua_players': pbac_joshua_players,
 		'cr_oldteams': cr_oldteams,
 		'old_wws_players': old_wws_players,
 		'cg_players': cg_players,
 		'ce_teams': ce_teams,
-		'noflores_dcn': noflores_dcn,
+		'flores_nodcn': flores_nodcn,
 		'sophia_leagues': sophia_leagues,
 		'sophia_teams': sophia_teams,
 		'football_players': football_players,
